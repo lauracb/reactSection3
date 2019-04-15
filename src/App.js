edaa509ac9d104475306f9ad7163e876b5a63266
lauracb <lauracadavidb@gmail.com>
2019-04-15 16:53:04 -0500

    Uso de props y props.children

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person /> 
+        <Person name="Lau" age="25"/> 
+        <Person name="Santy" age="21"> My Hobbies: Racing </Person>
+        <Person name="Thomy" age="11"/> 
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does it work?'));
